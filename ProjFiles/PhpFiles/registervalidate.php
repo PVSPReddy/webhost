517b41b3e979a51e92915d99b8fa23648c02008e
P.V.S.P.Reddy <vekatasivaprasadreddypulagam@gmail.com>
2017-04-09 17:05:49 +0530

    no message

--- ProjFiles/PhpFiles/registervalidate.php
+++ ProjFiles/PhpFiles/registervalidate.php
@@ -83,6 +83,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             }
         }
     }
+    echo ('<script language="javascript">');
+    echo ('alert(<?php $nameErr  ?>)');  //not showing an alert box.
+    echo ('</script>');
+
     if (empty($_POST["fileUploading"])) 
     {
         //$log_passwordErr = "Password is required";
@@ -102,11 +106,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
             $connn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO webhostdb.userinfo (uid, uname, password, first_name, last_name, mobile_no, email_id, profile_pic ) VALUES ('1', 'pvsivapr', '123456', 'Venkata Sivaprasad Reddy', 'Pulagam', '9865231458', 'pvsivapr@gmail.com', 'no pic' )";
             $connn->exec($sql);
-            echo "Connected successfully"; 
+            echo "Connected successfully";
+            echo ('<script language="javascript">');
+            echo ('alert(Connected successfully>)');  //not showing an alert box.
+            echo ('</script>');
         }
         catch(PDOException $e)
         {
             echo "Connection failed: " . $e->getMessage();
+            echo ('<script language="javascript">');
+            echo ('alert(<?php $e->getMessage();  ?>)');  //not showing an alert box.
+            echo ('</script>');
         }
     }
 }
