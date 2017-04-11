f9ecc10fc6e241dbae36532d9f38b96c8cef603d
P.V.S.P.Reddy <vekatasivaprasadreddypulagam@gmail.com>
2017-04-11 23:01:10 +0530

    committing changes

--- ProjFiles/JavaScript/check.php
+++ ProjFiles/JavaScript/check.php
@@ -6,32 +6,10 @@ $password = "siva_123456";
 $dbName = "webhostdb";
 $host = 3306;
 
-$shallAllow =false;
+$shallAllow =true;
 
 // define variables and set to empty values
 $log_Err ="";
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-    //echo ('step 0');
-    if (empty($_POST["logUName"])) 
-    {
-        $log_Err = "Name is required";
-        echo($log_Err);
-    } 
-    else
-    {
-        if (empty($_POST["logUPwd"]))
-        {
-            $log_Err = "Password is required";
-            echo($log_Err);
-        }
-        else
-        {
-            $shallAllow =true;
-            //echo "step 1";
-        }
-    }
-}
 
 if($shallAllow == true)
 {
@@ -39,31 +17,46 @@ if($shallAllow == true)
     {
         $connn = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
         $connn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "SELECT * from 'webhostdb.userinfo' WHERE uname="+"\""+$_POST["logUName"]+"\"";
+        $sql = "SELECT * from webhostdb.userinfo WHERE uname = 1";
+        //$sql = "SELECT * from 'webhostdb.userinfo' WHERE uid="+$_POST["logUName"];
+        //echo ($sql);
         
-        echo ($sql);
-        
+        /*
+        //1
+        $connn->exec($sql);
+        echo "Connected successfully";
+        echo "step 2";
+        $response1 = $connn->setFetchMode(PDO::FETCH_ASSOC); 
+        $response2 = $connn->fetchAll();
+       */
+       
+        //2
         // prepare sql and bind parameters
         $res_data = $connn->prepare($sql);
-        $res_data->exec($sql);
+        $res_data->execute();
+        
 
         // set the resulting array to associative
-        $response = $res_data->setFetchMode(PDO::FETCH_ASSOC); 
-        $_response = $res_data->fetchAll();
-       
+        $response1 = $res_data->setFetchMode(PDO::FETCH_ASSOC); 
+        $response2 = $res_data->fetchAll();
+        echo "step 4";
         
-        echo "\n"+"records obtained successfully";
-        $response[0][uname]
-        echo $response[0][password];
+        echo "\n"."records obtained successfully";
         //echo "\n"+$stmt;
         //echo "\n"+$result->userId;
         //echo "\n"+$result->firstName;
         //echo "\n"+$result->lastName;
         //echo "\n"+$result;
        // echo "\n"+$result2;
-                  
+       // print_r('response1:' . $response2['uname']);
+        //print_r('response2:' . $response2);            
         
-        if($response[0][password] == $_POST["logUPwd"])
+        foreach($response2 as $data)
+        {
+            echo $data['uname'];
+        }
+        
+        if($response2[0]['password'] == $_POST["logUPwd"])
         {
             print_r('user Login is valid');
         }
@@ -74,12 +67,14 @@ if($shallAllow == true)
     }
     catch(PDOException $e)
     {
-        echo "Connection failed: " + $e->getMessage();
+        echo('complete session');
+        echo 'Connection failed: ' . $e->getMessage();
         /*echo ('<script language="javascript">');
         echo ('alert(<?php $e->getMessage();  ?>)');  //not showing an alert box.
         echo ('</script>');
         */
     }
+    echo('end connection');
 }
 
 ?>
